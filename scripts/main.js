abd1446271ea583e90a070cfaf2abba7b5bf575d
mikael came <m.came@beapp.fr>
2017-06-05 20:07:43 +0200

    refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
+
+	// Tu crois vraiment que je balancerai le prénom de notre fille, comme ça ...
 	var secret = "7f481125cb09c0f7f91044996b90b7b7d29104d7";
 
 	var messagesError = ["Nope.", "Désolé ce n'est pas ca.",
 	"Retente ta chance.", "Quand même pas !",
-	"Oula... non pas du tout !", "Non, ce n'est pas ça.",
-	"Tu crois vraiment que je balancerai le prénom de notre fille, comme ça ..."
+	"Oula... non pas du tout !", "Non, ce n'est pas ça."
 	];
+
 	var tentatives=[];
 	define(function (require) {
 		var sha1 = require('./sha1');
@@ -22,13 +24,15 @@
 
 	firebase.initializeApp(config);
 
-	var solution = document.getElementById('solution');
+  // chargement solution depuis firebase
+	/*var solution = document.getElementById('solution');
 	var dbRef = firebase.database().ref().child("solution");
 	dbRef.on('value', snap => {
 		if(snap.value){
 			solution.innerText = snap.value
 		}
 	});
+*/
 
 
 	var test = function(name){
@@ -36,14 +40,14 @@
 		nameFormatte = name.charAt(0).toUpperCase() + name.substring(1).toLowerCase()
 		hash.update(nameFormatte);
 
-		if(hash==secret){
-			alert("Ouai !");
+		if(hash === secret){
+			alert("Oui ! C'est la bonne réponse!");
 		}
 		else{
 			showError();
-			document.getElementById('name').value='';
+			resetForm();
 			addTentatives(nameFormatte);
-
+			sendTentative(nameFormatte);
 		}
 	};
 
@@ -52,11 +56,28 @@
 		alert( messagesError[messagesErrorID]);
 	};
 
+	var resetForm = function(){
+		document.getElementById('name').value='';
+	};
+
 	var addTentatives = function(name){
+		// Affiches liste des tentatives
 		document.getElementById('tentatives').style.display="block";
+
+		// Ajoute une tentative à la liste
+		addTentative(name);
+
+		// Envoie firebase de la tentative
+		sendTentative(name);
+	};
+
+	var addTentative = function(name){
 		var newLI = document.createElement('li');
 		newLI.classList.add("list-group-item");
 		newLI.appendChild(document.createTextNode(name));
 		document.getElementById("listeTentatives").appendChild(newLI);
+	};
+
+	var sendTentative = function(name){
 
 	};
