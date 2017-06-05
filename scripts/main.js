595e0639a0ddb6ddfd9286f010ab262ea266f1a2
mikael came <m.came@beapp.fr>
2017-06-05 15:53:40 +0200

    Ajout tentatives

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,32 @@
 	"Retente ta chance.", "Quand même pas !",
 	"Oula... non pas du tout !", "Non, ce n'est pas ça.",
 	"Tu crois vraiment que je balancerai le prénom de notre fille, comme ça ..."
-];
-
+	];
+	var tentatives=[];
 	define(function (require) {
 		var sha1 = require('./sha1');
 	});
+
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyAbNR0hB2rYr8CPQvtsdi8SFXs4ivF25xA",
+		authDomain: "guessmyname-35287.firebaseapp.com",
+		databaseURL: "https://guessmyname-35287.firebaseio.com",
+		projectId: "guessmyname-35287",
+		storageBucket: "guessmyname-35287.appspot.com",
+		messagingSenderId: "653217500025"
+	};
+
+	firebase.initializeApp(config);
+
+	var solution = document.getElementById('solution');
+	var dbRef = firebase.database().ref().child("solution");
+	dbRef.on('value', snap => {
+		if(snap.value){
+			solution.innerText = snap.value
+		}
+	});
+
 
 	var test = function(name){
 		var hash = sha1.create();
@@ -20,10 +41,22 @@
 		}
 		else{
 			showError();
+			document.getElementById('name').value='';
+			addTentatives(nameFormatte);
+
 		}
 	};
 
 	var showError  = function(){
 		var messagesErrorID = Math.floor(Math.random() * messagesError.length);
 		alert( messagesError[messagesErrorID]);
-	}
+	};
+
+	var addTentatives = function(name){
+		document.getElementById('tentatives').style.display="block";
+		var newLI = document.createElement('li');
+		newLI.classList.add("list-group-item");
+		newLI.appendChild(document.createTextNode(name));
+		document.getElementById("listeTentatives").appendChild(newLI);
+
+	};
