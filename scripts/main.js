efb9151777cc38d9c8627b1c9d7ea28c613bbf4b
mikael came <m.came@beapp.fr>
2017-06-05 12:58:25 +0200

    modification encoding iso -> utf8

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,29 @@
-	var secret="7f481125cb09c0f7f91044996b90b7b7d29104d7";
-	
+	var secret = "7f481125cb09c0f7f91044996b90b7b7d29104d7";
+
+	var messagesError = ["Nope.", "Désolé ce n'est pas ca.",
+	"Retente ta chance.", "Quand même pas !",
+	"Oula... non pas du tout !", "Non, ce n'est pas ça.",
+	"Tu crois vraiment que je balancerai le prénom de notre fille, comme ça ..."
+];
+
 	define(function (require) {
 		var sha1 = require('./sha1');
-		
 	});
 
-	var test = function(name){ 
+	var test = function(name){
 		var hash = sha1.create();
 		nameFormatte = name.charAt(0).toUpperCase() + name.substring(1).toLowerCase()
 		hash.update(nameFormatte);
+
 		if(hash==secret){
-			alert("Ouai !"); 
+			alert("Ouai !");
 		}
 		else{
-			alert("Ce n'est pas ca..." ); 
-		}		
+			showError();
+		}
 	};
-	
+
+	var showError  = function(){
+		var messagesErrorID = Math.floor(Math.random() * messagesError.length);
+		alert( messagesError[messagesErrorID]);
+	}
