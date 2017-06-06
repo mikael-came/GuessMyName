f84715f4b22b1051509e51852adc88ba39dda358
CAME Mickael (SQLI) <xflf925@corp.log.intra.laposte.fr>
2017-06-06 14:51:47 +0200

    Integration hook.io pour recupertion des tentatives

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 
 	// Tu crois vraiment que je balancerai le prénom de notre fille, comme ça ...
 	var secret = "7f481125cb09c0f7f91044996b90b7b7d29104d7";
-
+	var username ='anonymous';
 	var messagesError = ["Nope.", "Désolé ce n'est pas ca.",
 	"Retente ta chance.", "Quand même pas !",
 	"Oula... non pas du tout !", "Non, ce n'est pas ça."
@@ -12,41 +12,22 @@
 		var sha1 = require('./sha1');
 	});
 
-	// Initialize Firebase
-	var config = {
-		apiKey: "AIzaSyAbNR0hB2rYr8CPQvtsdi8SFXs4ivF25xA",
-		authDomain: "guessmyname-35287.firebaseapp.com",
-		databaseURL: "https://guessmyname-35287.firebaseio.com",
-		projectId: "guessmyname-35287",
-		storageBucket: "guessmyname-35287.appspot.com",
-		messagingSenderId: "653217500025"
-	};
-
-	firebase.initializeApp(config);
-
-  // chargement solution depuis firebase
-	/*var solution = document.getElementById('solution');
-	var dbRef = firebase.database().ref().child("solution");
-	dbRef.on('value', snap => {
-		if(snap.value){
-			solution.innerText = snap.value
-		}
-	});
-*/
-
-
 	var test = function(name){
 		var hash = sha1.create();
-		nameFormatte = name.charAt(0).toUpperCase() + name.substring(1).toLowerCase()
-		hash.update(nameFormatte);
+		if(name){
+			
+			nameFormatte = name.charAt(0).toUpperCase() + name.substring(1).toLowerCase()
+			hash.update(nameFormatte);
 
-		if(hash === secret){
-			alert("Oui ! C'est la bonne réponse!");
-		}
-		else{
-			showError();
-			resetForm();
-			addTentatives(nameFormatte);
+			if(hash === secret){
+				alert("Oui ! C'est la bonne réponse!");
+			}
+			else{
+				showError();
+				resetForm();
+				addTentatives(nameFormatte);
+			}
+			
 			sendTentative(nameFormatte);
 		}
 	};
@@ -65,10 +46,7 @@
 		document.getElementById('tentatives').style.display="block";
 
 		// Ajoute une tentative à la liste
-		addTentative(name);
-
-		// Envoie firebase de la tentative
-		sendTentative(name);
+		addTentative(name);	
 	};
 
 	var addTentative = function(name){
@@ -78,6 +56,14 @@
 		document.getElementById("listeTentatives").appendChild(newLI);
 	};
 
-	var sendTentative = function(name){
-
+	var sendTentative = function(guess){
+		var oReq = new XMLHttpRequest();
+		var host = 'https://hook.io/mikael-came-gmail-com/storeguessmyname'
+		var url = host + "/" + username + "/" + guess;
+		oReq.open('POST', url);
+		oReq.send(null);
+		
+		oReq.onload = function () {
+			console.log('callBack, enregistrement done');
+		};
 	};
